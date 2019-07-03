efec6b4eb1b187de3dfeb5809aeaf572f01cad5e
Vincent <601521821@qq.com>
2019-07-03 18:59:29 +0800

    update commit

---
+++ hello-spring-cloud-web-admin-feign/src/main/java/com/example/hello/spring/cloud/web/admin/feign/controller/AdminController.java
@@ -0,0 +1,23 @@
+package com.example.hello.spring.cloud.web.admin.feign.controller;
+
+import com.example.hello.spring.cloud.web.admin.feign.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class AdminController {
+
+    @Resource
+    private AdminService adminService;
+
+    @RequestMapping(value = "hi", method = RequestMethod.GET)
+    public String sayHi(@RequestParam String message) {
+        return adminService.sayHi(message);
+    }
+}
